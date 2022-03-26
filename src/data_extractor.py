a6c598434101d62f4db019cd8b918a2a477ccc4f
Francisco Javier Serrano Arrese <fserranoarrese@twilio.com>
2022-03-26 19:20:05 +0100

    imports

--- src/data_extractor.py
+++ src/data_extractor.py
@@ -1,2 +1,16 @@
-# Extract and structure data from DB
+"""
+Extract and structure data from DB
+"""
+import os, pathlib
 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+import tensorflow_io as tfio
+
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras import layers, models
+from IPython import display
+
+print("Hola disculpa")
