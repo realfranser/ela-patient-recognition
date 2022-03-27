e6733d50688841e5440cd6923ca94af4073542b7
realfranser <serranoarresejavier@gmail.com>
2022-03-27 18:35:21 +0200

    waveforms ready

--- src/data_extractor.py
+++ src/data_extractor.py
@@ -17,7 +17,8 @@ tf.random.set_seed(SEED)
 np.random.seed(SEED)
 
 URL = "https://drive.upm.es/s/tDxI39MyWb0woIw/download"
-data_dir = pathlib.Path('data/Samples')
+PATH = 'data/Samples'
+data_dir = pathlib.Path(PATH)
 
 if not data_dir.exists():
     keras.utils.get_file(
@@ -36,7 +37,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 
 def open_audio_file(file=None):
     audio_bin = tf.io.read_file(file)
-    audio = tfio.audio.decode_flac(audio_bin, shape=None, dtype= tf.int16, name=None)
+    audio = tfio.audio.decode_wav(audio_bin, shape=None, dtype= tf.int16, name=None)
     audio_f = tf.cast(tf.squeeze(audio), dtype = tf.float32)
     audio_f = audio_f[:SAMPLES]
 
@@ -78,7 +79,7 @@ def plot_spectrogram(spectrogram, ax):
 def get_spectrogram_and_label_id(audio, label):
   spectrogram = get_spectrogram(audio)
   spectrogram = tf.expand_dims(spectrogram, -1)
-  label_id = tf.argmax(label==tipo_audio)
+  label_id = tf.argmax(label==audio_labels)
   return spectrogram, label_id
 
 def preprocess_dataset(files):
@@ -87,3 +88,59 @@ def preprocess_dataset(files):
   output_ds = output_ds.map(
       get_spectrogram_and_label_id,  num_parallel_calls=AUTOTUNE)
   return output_ds
+
+audio_labels = {
+	"pathology": 'ALS',
+	"control": 'HC'
+}
+train_data = PATH + '/HC/*'
+test_data = PATH + '/ALS/*'
+
+train_files = tf.io.gfile.glob(train_data)
+filenames_test = tf.io.gfile.glob(test_data)
+
+exp = len(train_files)
+num = round(exp/10)
+n_train = exp - num
+filenames_train = train_files[:n_train]
+filenames_validation = train_files[n_train:exp]
+
+filenames_train1 = tf.random.shuffle(filenames_train)
+filenames_test  = tf.random.shuffle(filenames_test)
+filenames_val   = tf.random.shuffle(filenames_validation)
+
+filenames_train = filenames_train1[:40]
+filenames_train2 = filenames_train1[40:]
+filenames_val = filenames_val[:4]
+filenames_test = filenames_test[:8]
+
+
+num_samples_train = len(filenames_train)
+num_samples_train2 = len(filenames_train2)
+num_samples_test  = len(filenames_test)
+num_samples_val  = len(filenames_val)
+
+print('Numero total de casos',exp)
+print('Numero total de casos para el entrenamiento del primer modelo:', num_samples_train)
+print('Numero total de casos para el entrenamiento del segundo modelo:', num_samples_train2 )
+print('Numero total de casos para test:', num_samples_test)
+print('Numero total de casos para validacion:', num_samples_val)
+print('Etiquetas: ', audio_labels)
+
+files_ds  = tf.data.Dataset.from_tensor_slices(filenames_train)
+waveform_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
+
+rows = 3
+cols = 3
+n = rows*cols
+fig, axes = plt.subplots(rows, cols, figsize=(18, 20))
+for i, (audio, label) in enumerate(waveform_ds.take(n)):
+  r = i // cols
+  c = i % cols
+  ax = axes[r][c]
+  ax.plot(audio.numpy())
+  ax.set_yticks(np.arange(-40, 45, 10))
+  label = label.numpy().decode('utf-8')
+  ax.set_title(label)
+
+plt.show()
